e60836074b401106c3fd9a76e6b4414a5889cc20
Daniel Jorge <daniel.jorge@cinnecta,com>
2020-05-16 16:55:01 -0300

    using callbacks for forms

--- my-app/src/components/table/Table.js
+++ my-app/src/components/table/Table.js
@@ -1,45 +1,46 @@
 import React from 'react';
+import FormTable from './form/TableForm';
 import './Table.css';
 
 const Header = () => (
-    <div className='header'>
-        <div>Names</div>
-        <div>Age</div>
-        <div></div>
-    </div>
-)
-
-const Body = ({ data, onDeleteUser }) => (
-    <div className='body'>
-        {data.map((user, index) => (
-            <Row
-                user={user}
-                onDeleteUser={onDeleteUser}
-                key={index} />
-        ))}
-    </div>
-)
-
-const Row = ({ user, onDeleteUser }) => (
-    <div className='row'>
-        <div>{user.name}</div>
-        <div>{user.age}</div>
-        <button onClick={() => onDeleteUser(user)}>remove</button>
-    </div>
+  <div className='header'>
+    <div>Names</div>
+    <div>Age</div>
+    <div>Actions</div>
+  </div>
 );
 
+const Body = ({ data, onDeleteUser, onHandleSubmit }) => (
+  <div className='body'>
+    <FormTable onHandleSubmit={onHandleSubmit} />
+
+    {data.map((user, index) => (
+      <Row user={user} onDeleteUser={onDeleteUser} key={index} />
+    ))}
+  </div>
+);
+
+const Row = ({ user, onDeleteUser }) => (
+  <div className='row'>
+    <div>{user.name}</div>
+    <div>{user.age}</div>
+    <button onClick={() => onDeleteUser(user)}>remove</button>
+  </div>
+);
 
 function Table(props) {
-    const { data, onDeleteUser } = props;
+  const { data, onDeleteUser, onHandleSubmit } = props;
 
-    return (
-        <div className='table'>
-            <Header />
-            <Body
-                data={data}
-                onDeleteUser={onDeleteUser} />
-        </div>
-    );
+  return (
+    <div className='table'>
+      <Header />
+      <Body
+        data={data}
+        onDeleteUser={onDeleteUser}
+        onHandleSubmit={onHandleSubmit}
+      />
+    </div>
+  );
 }
 
 export default Table;
