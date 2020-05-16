ba916172a4d98fc52f85c0c12c64cdc8c5604d3a
Daniel Jorge <djpgonzaga@gmail.com>
2020-05-16 00:45:02 -0300

    using useState hook

--- my-app/src/components/table/Table.js
+++ my-app/src/components/table/Table.js
@@ -5,33 +5,39 @@ const Header = () => (
     <div className='header'>
         <div>Names</div>
         <div>Age</div>
+        <div></div>
     </div>
 )
 
-const Body = ({ data }) => (
+const Body = ({ data, onDeleteUser }) => (
     <div className='body'>
         {data.map((user, index) => (
-            <Row user={user}
+            <Row
+                user={user}
+                onDeleteUser={onDeleteUser}
                 key={index} />
         ))}
     </div>
 )
 
-const Row = ({ user }) => (
+const Row = ({ user, onDeleteUser }) => (
     <div className='row'>
         <div>{user.name}</div>
         <div>{user.age}</div>
+        <button onClick={() => onDeleteUser(user)}>remove</button>
     </div>
 );
 
 
 function Table(props) {
-    const { data } = props;
+    const { data, onDeleteUser } = props;
 
     return (
         <div className='table'>
             <Header />
-            <Body data={data} />
+            <Body
+                data={data}
+                onDeleteUser={onDeleteUser} />
         </div>
     );
 }
