64c62e51e8d258a8784021093be2a09ca8d2dab8
Daniel Jorge <djpgonzaga@gmail.com>
2020-05-16 16:55:01 -0300

    using callbacks for forms

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,28 @@
 import React, { useState } from 'react';
 import './App.css';
-import Table from './components/table/Table'
-import mock from './utils/tableMock.json'
+import Table from './components/table/Table';
+import mock from './utils/tableMock.json';
 
 const DATA_DEFAULT = mock;
 
 function App() {
-
-  const [data, setData] = useState(DATA_DEFAULT)
+  const [data, setData] = useState(DATA_DEFAULT);
 
   function onDeleteUser(id) {
-    setData(data.filter((user) => user.id !== id))
+    setData(data.filter((user) => user.id !== id));
+  }
+
+  function onHandleSubmit(user) {
+    setData([...data, { ...user, id: data[data.length - 1].id + 1 }]);
   }
 
   return (
-    <div className="container">
+    <div className='container'>
       <Table
         data={data}
-        onDeleteUser={({id}) => onDeleteUser(id)} />
+        onDeleteUser={({ id }) => onDeleteUser(id)}
+        onHandleSubmit={(user) => onHandleSubmit(user)}
+      />
     </div>
   );
 }
