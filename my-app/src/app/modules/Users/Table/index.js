36a32ca559044e949de3045957a1032359498390
Daniel Jorge <daniel.jorge@cinnecta,com>
2020-05-24 22:08:49 -0300

    create generic api service for fetch data

--- my-app/src/app/modules/Users/Table/index.js
+++ my-app/src/app/modules/Users/Table/index.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState, memo } from 'react';
-import Proptypes from 'prop-types';
 import Body from './Body';
+import Api from '../../../common/services/api';
 import './styles.css';
+
+const usersDefault = [];
+const endpoint = 'employees';
 
 const Header = () => (
   <div className="header">
@@ -13,50 +16,51 @@ const Header = () => (
 
 const MemoHeader = memo(Header);
 
-const propTypes = {
-  data: Proptypes.arrayOf(
-    Proptypes.shape({
-      name: Proptypes.string,
-      age: Proptypes.number,
-      id: Proptypes.number,
-    }),
-  ).isRequired,
-};
-
-const userDataDefault = [];
-
-function Table(props) {
-  const { data } = props;
-
-  const [userData, setUserData] = useState(userDataDefault);
+function Table() {
+  const [users, setUsers] = useState(usersDefault);
 
   useEffect(() => {
-    setUserData(data);
-  }, [data]);
+    Api.get(endpoint)
+      .then((employees) => {
+        const dataParse = employees && employees.data.map((employee) => ({
+          name: employee.employee_name,
+          age: Number(employee.employee_age),
+          id: Number(employee.id),
+        }));
+
+        setUsers(dataParse);
+      });
+  }, []);
 
   function onDeleteUser(deleteUser) {
-    setUserData(userData.filter((user) => user.id !== deleteUser.id));
+    setUsers(users.filter((user) => user.id !== deleteUser.id));
+    // Dummy rest api example dont's work with delete
+    Api.delete('delete', deleteUser.id);
   }
 
   function onHandleSubmit(user) {
-    setUserData([...userData, {
+    setUsers([...users, {
       ...user,
-      id: (userData.length > 0 && userData[userData.length - 1].id) + 1,
+      id: (users.length > 0 && users[users.length - 1].id) + 1,
     }]);
+
+    // Dummy rest api example dont's work with create
+    Api.post('create', JSON.stringify({
+      employee_name: user.name,
+      employee_age: user.age,
+    }));
   }
 
   return (
     <div className="table">
       <MemoHeader />
       <Body
-        data={userData}
+        data={users}
         onDeleteUser={onDeleteUser}
         onHandleSubmit={onHandleSubmit}
       />
     </div>
   );
 }
-
-Table.propTypes = propTypes;
 
 export default Table;
